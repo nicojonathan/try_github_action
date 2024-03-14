24db52c95dc4691ac0c427ced8a684b51bb68087
nicojonathan <nicojonathan69@gmail.com>
2024-03-14 15:32:53 +0700

    corrected the error in response_controller line L15 L24

--- controllers/response_controller.go
+++ controllers/response_controller.go
@@ -6,23 +6,37 @@ import (
 	m "review_utk_uts/models"
 )
 
-func sendUserSuccessResponse (w http.ResponseWriter, message string, data []m.User) {
-	var response m.UsersResponse
-	response.Status = 200
-	response.Message = message
-	response.Data = data
-	w.Header().Set("Content-Type", "application/json")
-	json.NewEncoder(w).Encode(response)
+func sendUserSuccessResponse(w http.ResponseWriter, message string, data []m.User) {
+    var response m.UsersResponse
+    response.Status = 200
+    response.Message = message
+    response.Data = data
+
+    w.Header().Set("Content-Type", "application/json")
+    err := json.NewEncoder(w).Encode(response)
+    if err != nil {
+        // Handle the error appropriately, for example:
+        http.Error(w, "Error encoding JSON response", http.StatusInternalServerError)
+        return
+    }
 }
 
-func sendTransactionDetailSuccessResponse (w http.ResponseWriter, message string, data []m.TransactionDetail) {
-	var response m.TransactionDetailResponse
-	response.Status = 200
-	response.Message = message
-	response.Data.Transaction = data
-	w.Header().Set("Content-Type", "application/json")
-	json.NewEncoder(w).Encode(response)
+
+func sendTransactionDetailSuccessResponse(w http.ResponseWriter, message string, data []m.TransactionDetail) {
+    var response m.TransactionDetailResponse
+    response.Status = 200
+    response.Message = message
+    response.Data.Transaction = data
+
+    w.Header().Set("Content-Type", "application/json")
+    err := json.NewEncoder(w).Encode(response)
+    if err != nil {
+        // Handle the error appropriately, for example:
+        http.Error(w, "Error encoding JSON response", http.StatusInternalServerError)
+        return
+    }
 }
+
 
 func sendSuccessResponse (w http.ResponseWriter, message string) {
 	var response m.GeneralResponse
